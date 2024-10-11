2e0f4cba81c4ce34f94ce3cff3302180b9752de5
Hampus-CS <150423568+Hampus-CS@users.noreply.github.com>
2024-10-11 12:12:36 +0200

    Update Invader.cs
    Changed from 2 to 4 sprites for animation.

--- Assets/Scripts/Invader.cs
+++ Assets/Scripts/Invader.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 
 public class Invader : MonoBehaviour
 {
-    public Sprite[] animationSprites = new Sprite[2];
+    public Sprite[] animationSprites = new Sprite[4];
     public float animationTime;
 
     SpriteRenderer spRend;
