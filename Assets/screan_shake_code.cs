5465a7b7b0f22f634f7866fb029f971c67528332
Tuddebud <150425823+Tuddebud@users.noreply.github.com>
2024-10-15 12:05:19 +0200

    screnshake 1

---
+++ Assets/screan_shake_code.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screan_shake_code : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float shake = 0f;
+    
+    void Start()
+    {
+        //shake = 5;
+        GameObject.Find("Main Camera").GetComponent<screan_shake_code>().shake = 2f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(Random.Range(-shake, shake), Random.Range(-shake, shake), -10);
+        if (shake > 0f) shake -= Time.deltaTime * 10f;
+        shake = Mathf.Clamp(shake, 0, 100);
+    }
+}
