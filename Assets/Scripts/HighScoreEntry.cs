ab2f2cd8d2be5ea240e20481b7c4f8e784a40e06
Hampus-CS <150423568+Hampus-CS@users.noreply.github.com>
2024-10-24 13:34:34 +0200

    Score system fully functional
    Score system is complete and ready for implementation into the main part of the game.

--- Assets/Scripts/HighScoreEntry.cs
+++ Assets/Scripts/HighScoreEntry.cs
@@ -1,5 +1,16 @@
+using UnityEngine;
+using System.Xml.Serialization;
+
 public class HighScoreEntry
 {
-    public string name;
-    public int score;
+    public int Score;
+    public string Nickname;
+
+    public HighScoreEntry() { } // Parameterlös konstruktör för XML-serialisering
+
+    public HighScoreEntry(int score, string nickname)
+    {
+        Score = score;
+        Nickname = nickname;
+    }
 }
